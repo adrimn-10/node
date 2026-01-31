379839edc41553eb59e614dcdf65073450543d68
adrimn-10 <adriftv999@gmail.com>
2026-01-31 09:44:26 -0500

    Import app.js and close DB connection in tests
    Added database connection closure after tests.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,13 @@
-import app from "../src/app";
 import request from "supertest";
+import app from "../src/app.js"; // Asegúrate de incluir el .js
+import { pool } from "../src/db.js"; // Importamos el pool para poder cerrarlo
 
 describe("Index Routes", () => {
+  // Cerramos la conexión a la base de datos al terminar
+  afterAll(async () => {
+    await pool.end();
+  });
+
   it("should respond welcome", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
