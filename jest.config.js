b2381b2c3bb9ba24c45eab1170f4a1e883a53ac1
adrimn-10 <adriftv999@gmail.com>
2026-01-31 06:59:16 -0500

    Set minimum coverage thresholds for CI
    Added coverage threshold settings for CI requirements.

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,14 @@ export default {
   testEnvironment: "node",
   coveragePathIgnorePatterns: ["/node_modules/"],
   transform: {},
+
+  // Cobertura mínima obligatoria para CI
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
